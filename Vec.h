6490b2275171c4d166191ef7416cefe4bba085a9
Tastyep <luc.sinet@epitech.eu>
2016-05-22 01:10:25 +0200

    Added methods back, pop_back, at, size and created a new file Vecp that will contain the same macro but working for pointers

--- Vec.h
+++ Vec.h
@@ -12,18 +12,30 @@ struct {\
 }
 
 #define vec_init(vec)\
-(vec)->data = NULL; (vec)->size = 0; (vec)->capacity = 0;
+(vec).data = NULL; (vec).size = 0; (vec).capacity = 0;
 
 #define vec_attr(vec)\
-(char **)(&(vec)->data), &(vec)->size, &(vec)->capacity
+(char **)(&(vec).data), &(vec).size, &(vec).capacity
 
 #define data_size(vec)\
-sizeof(*((vec)->data))
+sizeof(*((vec).data))
 
 #define vec_push_back(vec, value)\
     vec_expand(vec_attr(vec), data_size(vec));\
-    (vec)->data[(vec)->size - 1] = value;\
-    ++(vec)->size;
+    (vec).data[(vec).size] = value;\
+    ++(vec).size;
+
+#define vec_pop_back(vec)\
+    --(vec).size;
+
+#define vec_back(vec)\
+    (vec).data[(vec).size]
+
+#define vec_at(vec, idx)\
+    (vec).data[idx]
+
+#define vec_size(vec)\
+    (vec).size
 
 int vec_expand(char **data, size_t *size, size_t *capacity, unsigned int data_size);
 
--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "Vec.h"
+#include <stdio.h>
 
 int main(void) {
  Vec(int) intVec;
  
- vec_init(&intVec);
- vec_push_back(&intVec, 5);
+ vec_init(intVec);
+ vec_push_back(intVec, 5);
+ 
+ for (unsigned int i = 0; i < vec_size(intVec); ++i) {
+  printf("%d\n", vec_at(intVec, i));
+ }
  return 0;
 }
